23f9b87aca6c9bd6a4e82c00b1ed7f8d5d563916
jhscxz <jhsc.xz@gmail.com>
2025-04-01 22:04:34 +0200

    numerirano Srodstvo.cs

--- Enums/Srodstvo.cs
+++ Enums/Srodstvo.cs
@@ -3,16 +3,16 @@ namespace DodjelaStanovaZG.Enums;
 public enum Srodstvo
 {
     PodnositeljZahtjeva = 1,
-    BracniDrug,
-    IzvanbracniDrug,
-    ZivotniPartner,
-    NeformalniZivotniPartner,
-    SrodnikPoKrvnojLiniji,
-    SrodnikPoPobocnojLiniji,
-    PartnerSrodnika,
-    ZivotniIliNeformalniPartnerSrodnika,
-    Pastorak,
-    Posvojenik,
-    Posvojitelj,
-    UzdrzavaniClan
+    BracniDrug = 2,
+    IzvanbracniDrug = 3,
+    ZivotniPartner = 4,
+    NeformalniZivotniPartner = 5,
+    SrodnikPoKrvnojLiniji = 6,
+    SrodnikPoPobocnojLiniji = 7,
+    PartnerSrodnika = 8,
+    ZivotniIliNeformalniPartnerSrodnika = 9,
+    Pastorak = 10,
+    Posvojenik = 11,
+    Posvojitelj = 12,
+    UzdrzavaniClan = 13
 }
