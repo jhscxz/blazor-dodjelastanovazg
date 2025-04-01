8a1b95b5a0a097d329bf9dd64982c768a8e94455
jhscxz <jhsc.xz@gmail.com>
2025-04-01 19:15:11 +0200

    dodan home

---
+++ Areas/Pocetna/Pocetna.razor.cs
@@ -0,0 +1,35 @@
+using DodjelaStanovaZG.Components.UI;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace DodjelaStanovaZG.Areas.Pocetna;
+
+public class HomePageBase : ComponentBase
+{
+    protected List<Breadcrumbs.BreadcrumbItem> BreadcrumbItems { get; } =
+    [
+        new Breadcrumbs.BreadcrumbItem
+        {
+            Text = "Početna",
+            CssClass = "text-red-500 font-bold",
+        }
+    ];
+
+    protected List<(string Title, string Description, string NavigateTo, string ButtonText, string Icon)> HomeCards =>
+    [
+        (
+            "Socijalni natječaji",
+            "Obrada socijalnog natječaja.",
+            "/socijalni-natjecaj",
+            "Idi na socijalne natječaje",
+            Icons.Material.Filled.List
+        ),
+        (
+            "Priuštivi natječaji",
+            "Obrada priuštivog natječaja.",
+            "/priuštivi-natjecaj",
+            "Idi na priuštive natječeje",
+            Icons.Material.Filled.List
+        )
+    ];
+}
