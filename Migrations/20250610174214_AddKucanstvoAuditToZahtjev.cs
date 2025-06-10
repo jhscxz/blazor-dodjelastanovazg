266a4ee099fee4b010bc7798a9df4dc7eaad5b7e
jhscxz <jhsc.xz@gmail.com>
2025-06-10 20:23:44 +0200

    isto dodatak svasta nesta

---
+++ Migrations/20250610174214_AddKucanstvoAuditToZahtjev.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DodjelaStanovaZG.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddKucanstvoAuditToZahtjev : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "KucanstvoUpdatedAt",
+                table: "SocijalniNatjecajZahtjevi",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "KucanstvoUpdatedBy",
+                table: "SocijalniNatjecajZahtjevi",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "KucanstvoUpdatedAt",
+                table: "SocijalniNatjecajZahtjevi");
+
+            migrationBuilder.DropColumn(
+                name: "KucanstvoUpdatedBy",
+                table: "SocijalniNatjecajZahtjevi");
+        }
+    }
+}
