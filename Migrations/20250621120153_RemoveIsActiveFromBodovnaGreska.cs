5b6f7f96126392c34ad48aa4fd6a2dda684ebeb4
jhscxz <jhsc.xz@gmail.com>
2025-06-21 14:11:21 +0200

    refactor, uklonjen IsActive atribut iz BodovnaGreska migracije

---
+++ Migrations/20250621120153_RemoveIsActiveFromBodovnaGreska.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DodjelaStanovaZG.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveIsActiveFromBodovnaGreska : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "SocijalniNatjecajBodovnaGreske");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "SocijalniNatjecajBodovnaGreske",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
